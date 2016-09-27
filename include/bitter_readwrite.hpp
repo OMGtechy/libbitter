6531a8a6a062bebcb81b514a8d06bd1d8b9286a7
Joshua Gerrard <enquiries@joshuagerrard.com>
2016-09-27 21:21:48 +0100

    Wrote a test to check that a null byte had all bits returned as Bit::Zero

---
+++ include/bitter_readwrite.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace bitter {
+    enum class Bit {
+        Zero = 0,
+        One = 1
+    };
+
+    class BitReader {
+    public:
+        BitReader(const char* const source, const size_t sourceBytes)
+        : m_source(source),
+          m_sourceBytes(sourceBytes) {
+
+        }
+
+
+        Bit getBitNumber(const size_t bitNumber) const {
+            return Bit::Zero;
+        }
+
+    private:
+        const char* const m_source;
+        const size_t m_sourceBytes;
+    };
+}
+
---
+++ test/source/test_bitter_readwrite.cpp
@@ -0,0 +1,31 @@
+#include <catch.hpp>
+
+#include <bitter_readwrite.hpp>
+
+namespace bitter {
+    namespace test {
+        SCENARIO("bits can be read from a target") {
+            GIVEN("a single byte with all zeros") {
+                const char byte = 0b00000000;
+
+                WHEN("when a bit reader is created to read it") {
+                    BitReader bitReader(&byte, sizeof(byte));
+
+                    WHEN("bits 0 through 7 (inclusive) are read") {
+                        THEN("it should always return Bit::Zero") {
+                            REQUIRE(bitReader.getBitNumber(0) == Bit::Zero);
+                            REQUIRE(bitReader.getBitNumber(1) == Bit::Zero);
+                            REQUIRE(bitReader.getBitNumber(2) == Bit::Zero);
+                            REQUIRE(bitReader.getBitNumber(3) == Bit::Zero);
+                            REQUIRE(bitReader.getBitNumber(4) == Bit::Zero);
+                            REQUIRE(bitReader.getBitNumber(5) == Bit::Zero);
+                            REQUIRE(bitReader.getBitNumber(6) == Bit::Zero);
+                            REQUIRE(bitReader.getBitNumber(7) == Bit::Zero);
+                        } 
+                    }
+                }
+            }
+        } 
+    }
+}
+
