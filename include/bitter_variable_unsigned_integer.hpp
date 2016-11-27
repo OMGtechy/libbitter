186f6e0c6388a8b8a197542694edf0e629ce88f4
Joshua Gerrard <enquiries@joshuagerrard.com>
2016-11-27 13:48:01 +0000

    Documented operator> for VariableUnsignedIntegers and unsigned primitives

--- include/bitter_variable_unsigned_integer.hpp
+++ include/bitter_variable_unsigned_integer.hpp
@@ -514,6 +514,50 @@ namespace bitter {
     bool operator>(const VariableUnsignedInteger& lhs, const VariableUnsignedInteger& rhs);
 
     //!
+    //! \brief  Checks if a VariableUnsignedInteger is greater than an unsigned primitive
+    //!
+    //! \tparam  T  the type of the unsigned primitive
+    //!
+    //! \param[in]  lhs  the left operand
+    //! \param[in]  rhs  the right operand
+    //!
+    //! \returns  true if \p lhs is greater than \p rhs, false otherwise
+    //!
+    //! \par Example
+    //! \code
+    //!     // given an VariableUnsignedInteger called x
+    //!     const bool xIsGreaterThan42 = x > 42;
+    //! \endcode
+    //!
+    //! \relates  VariableUnsignedInteger
+    //!
+    template <typename T,
+              typename = std::enable_if<std::is_unsigned<T>::value>>
+    bool operator>(const VariableUnsignedInteger& lhs, const T& rhs);
+
+    //!
+    //! \brief  Checks if an unsigned primitive is greater than a VariableUnsignedInteger
+    //!
+    //! \tparam  T  the type of the unsigned primitive
+    //!
+    //! \param[in]  lhs  the left operand
+    //! \param[in]  rhs  the right operand
+    //!
+    //! \returns  true if \p lhs is greater than \p rhs, false otherwise
+    //!
+    //! \par Example
+    //! \code
+    //!     // given an VariableUnsignedInteger called x
+    //!     const bool fourtyTwoIsGreaterThanX = 42 < x;
+    //! \endcode
+    //!
+    //! \relates  VariableUnsignedInteger
+    //!
+    template <typename T,
+              typename = std::enable_if<std::is_unsigned<T>::value>>
+    bool operator>(const T& lhs, const VariableUnsignedInteger& rhs);
+
+    //!
     //! \brief  Checks if one VariableUnsignedInteger is greater than or equal to another
     //!
     //! \param[in]  lhs  the left operand
