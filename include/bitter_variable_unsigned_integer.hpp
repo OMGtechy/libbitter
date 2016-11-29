34900f0e4b6cfc9a29ca0f32ede33abe66179259
Joshua Gerrard <enquiries@joshuagerrard.com>
2016-11-29 21:22:45 +0000

    Documented unsigned integer overloads of VariableUnsignedInteger operator>=

--- include/bitter_variable_unsigned_integer.hpp
+++ include/bitter_variable_unsigned_integer.hpp
@@ -618,6 +618,50 @@ namespace bitter {
     //! \relates  VariableUnsignedInteger
     //!
     bool operator>=(const VariableUnsignedInteger& lhs, const VariableUnsignedInteger& rhs);
+
+    //!
+    //! \brief  Checks if a VariableUnsignedInteger is greater than or equal to an unsigned primitive
+    //!
+    //! \tparam  T  the type of the unsigned primitive
+    //!
+    //! \param[in]  lhs  the left operand
+    //! \param[in]  rhs  the right operand
+    //!
+    //! \returns  true if \p lhs is greater than or equal to \p rhs, false otherwise
+    //!
+    //! \par Example
+    //! \code
+    //!     // given an VariableUnsignedInteger called x
+    //!     const bool xIsGreaterThanOrEqualTo42 = x >= 42;
+    //! \endcode
+    //!
+    //! \relates  VariableUnsignedInteger
+    //!
+    template <typename T,
+              typename = std::enable_if<std::is_unsigned<T>::value>>
+    bool operator>=(const VariableUnsignedInteger& lhs, const T& rhs);
+
+    //!
+    //! \brief  Checks if an unsigned primitive is greater than or equal to a VariableUnsignedInteger
+    //!
+    //! \tparam  T  the type of the unsigned primitive
+    //!
+    //! \param[in]  lhs  the left operand
+    //! \param[in]  rhs  the right operand
+    //!
+    //! \returns  true if \p lhs is greater than or equal to \p rhs, false otherwise
+    //!
+    //! \par Example
+    //! \code
+    //!     // given an VariableUnsignedInteger called x
+    //!     const bool fourtyTwoIsGreaterThanOrEqualToX = 42 >= x;
+    //! \endcode
+    //!
+    //! \relates  VariableUnsignedInteger
+    //!
+    template <typename T,
+              typename = std::enable_if<std::is_unsigned<T>::value>>
+    bool operator>=(const T& lhs, const VariableUnsignedInteger& rhs);
 }
 
 ///
